fa9673c727b0236a6e45df4159b2d4fbf4a84986
Giorgian Borca-Tasciuc <giorgian.borca-tasciuc@protonmail.com>
2020-01-15 13:21:48 -0500

    Added script to visualize hdf5 files

---
+++ scripts/plot-3d-h5.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import h5py
+import sys
+from mayavi import mlab
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+with h5py.File(sys.argv[1], 'r') as f:
+    print("Available keys:")
+    for key in f.keys():
+        print(key)
+
+    data = np.array(f[sys.argv[2]])
+    s = mlab.contour3d(data, colormap="YlGnBu")
+    mlab.show()
+
+   
+
